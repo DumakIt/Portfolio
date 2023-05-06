75865ef89d548852291ed1525c3bba0a3bd34bd5
DumakIt <odumak4411@gmail.com>
2023-05-06 23:33:01 +0900

    230506 그림자 아티팩트 오류 수정중

--- src/components/units/threeMain/threeMain.tsx
+++ src/components/units/threeMain/threeMain.tsx
@@ -1,15 +1,57 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  OrbitControls,
+  RandomizedLight,
+  Sky,
+  SoftShadows,
+  Stars,
+  useHelper,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+import { DirectionalLightHelper, SpotLightHelper } from "three";
 import ThreeModel from "../../commons/threeModel/threeModel";
 import { Container } from "./threeMainStyles";
 
 export default function ThreeMain(): JSX.Element {
+  const Aaa = () => {
+    const light = useRef();
+    useHelper(light, DirectionalLightHelper, 1, "red");
+
+    return (
+      <directionalLight
+        ref={light}
+        castShadow
+        position={[-70, 30, 40]}
+        shadow-mapSize={[8192, 8192]}
+        shadow-camera-left={-100}
+        shadow-camera-right={100}
+        shadow-camera-top={100}
+        shadow-camera-bottom={-100}
+        shadow-bias={-0.0013}
+      />
+    );
+  };
+
   return (
     <Container>
-      <Canvas>
+      <Canvas
+        camera={{
+          position: [-19, 10, 19],
+        }}
+        shadows
+      >
+        <mesh>
+          {/* <Sky sunPosition={[1, 0.2, 1]} /> */}
+          {/* <Stars count={5000} /> */}
+        </mesh>
         <OrbitControls />
-        <ambientLight />
+        <ambientLight intensity={0.3} />
+        {/* <directionalLight position={[-30, 20, 30]} castShadow /> */}
+        <Aaa />
         <ThreeModel />
+        <gridHelper />
+        <axesHelper />
       </Canvas>
     </Container>
   );
